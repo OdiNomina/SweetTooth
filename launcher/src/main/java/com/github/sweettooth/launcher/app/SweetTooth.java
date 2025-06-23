363533dc0362710a747e2dbe1890227a6d6950f6
Silke Dittmar <silkedittmar.ae@outlook.de>
2025-06-23 12:49:00 +0200

    Upgrade main()
    - add logging initialization
    - add default uncaught exception handler
    - add shutdown hook
    - update exception handling

--- launcher/src/main/java/com/github/sweettooth/launcher/app/SweetTooth.java
+++ launcher/src/main/java/com/github/sweettooth/launcher/app/SweetTooth.java
@@ -2,46 +2,73 @@ package com.github.sweettooth.launcher.app;
 
 import com.github.sweettooth.controller.api.ControllerInterface;
 
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Locale;
+import java.util.logging.Logger;
+import java.util.logging.LogManager;
 
 import com.github.sweettooth.controller.api.ControllerFactory;
 import com.github.sweettooth.model.api.GameModelInterface;
-import com.github.sweettooth.model.api.Logged;
 import com.github.sweettooth.model.api.Settings;
+import com.github.sweettooth.shared.api.Loggable;
+import com.github.sweettooth.shared.api.LoggingSetup;
 import com.github.sweettooth.view.api.DisplayElement;
 import com.github.sweettooth.view.api.DisplayFactory;
 
-public class SweetTooth implements Logged {
-	static Logged logged = new SweetTooth();
+public class SweetTooth implements Loggable {
+	private static final Logger LOGGER = Logger.getLogger(SweetTooth.class.getName());
+	private static SweetTooth app;
 	
 	SweetTooth(){}
 	
 	public static void main(String[] args) {
-		Instant start = Instant.now();
-		Settings settings = new Settings(Locale.GERMANY);
-		
 		try {
+			Instant start = Instant.now();
+			app = new SweetTooth();
+			LoggingSetup.initialize(SweetTooth.class);
+			app.setDefaultUncaughtExceptionHandler();
+			app.addShutdownHook();
+			
+			Settings settings = new Settings(Locale.GERMANY);
 			GameModelInterface gameModel = GameModelInterface.createGameModel();
-			gameModel.initialize(settings, null);
-			
 			ControllerInterface lanternaController = ControllerFactory.create();
-			lanternaController.initialize(gameModel);
-			
 			DisplayElement lanternaGUI = DisplayFactory.create();
-			lanternaGUI.initialize(gameModel, lanternaController, settings);
+			try {
+				gameModel.initialize(settings, null);
+				lanternaController.initialize(gameModel);
+				lanternaGUI.initialize(gameModel, lanternaController, settings);
+			}
+			catch(RuntimeException e) { app.error(e.getClass().getName() + " during initialization of modules.", e); }
 			
-			Thread guiThread = new Thread(lanternaGUI);
-			guiThread.start();
-	    }
-		catch(RuntimeException e) {
-			logged.warn(null, e);
+			new Thread(lanternaGUI, "CreateLanternaGUI").start();
+			app.info(String.format(Thread.currentThread().getName() + " thread stopped: Runtime %s ms", start.until(Instant.now(), ChronoUnit.MILLIS)));
 		}
-	    catch (Exception e) {
-	    	logged.warn(null, e);
-	    }
-		
-		logged.info(String.format(Thread.currentThread() + " stopped: Runtime %s ms", start.until(Instant.now(), ChronoUnit.MILLIS)));
+		catch(Exception e) { app.error(Thread.currentThread().getName() + " thread throws " + e.getClass().getName(), e); }
+	}
+
+	private void addShutdownHook() {
+		try {
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				app.info("Shutdown hook is executed - Logger is reset.");
+		        try { LogManager.getLogManager().reset(); } // Schließt alle globalen Handler (z.B. FileHandler)
+		        catch(SecurityException e) { error(e.getClass().getName() + " when attempting to reset log manager.", e); }
+		    }));
+		}
+		catch(IllegalArgumentException | IllegalStateException | SecurityException e) { error(e.getClass().getName() + " when adding 'shutdown hook'.", e); }
+	}
+	
+	@Override
+	public Logger getLogger() {
+		return LOGGER;
+	}
+	
+	private void setDefaultUncaughtExceptionHandler() {
+		UncaughtExceptionHandler ueh = (thread, exception) -> {
+			error("Uncaught exception in thread '" + thread.getName() + "': ", exception);
+		};
+		try { Thread.setDefaultUncaughtExceptionHandler(ueh); }
+		catch(SecurityException e) { error(e.getClass().getName() + " when setting 'default uncaught exception handler'.", e); }
 	}
 }
