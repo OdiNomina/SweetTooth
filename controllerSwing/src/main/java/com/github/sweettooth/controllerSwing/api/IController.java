e345f3b73f48639a4fada247f806f9a568407273
Silke Dittmar <silkedittmar.ae@outlook.de>
2025-09-17 10:11:42 +0200

    Implement swing controller API.

--- controllerSwing/src/main/java/com/github/sweettooth/controllerSwing/api/IController.java
+++ controllerSwing/src/main/java/com/github/sweettooth/controllerSwing/api/IController.java
@@ -2,6 +2,8 @@ package com.github.sweettooth.controllerSwing.api;
 
 import com.github.sweettooth.model.api.IGameData;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -17,4 +19,34 @@ public interface IController {
 	
 	IController initialize(IGameData gameModel) throws NullPointerException;
 	
+	/**
+	 * Creates the matching text field listener for the event.
+	 * @param eventName the name of the corresponding event.
+	 * @param associatedComboBox the content of this combo box can be used.
+	 * @param nextInFocus the component that gets the focus after a valid input.
+	 * @param answerBox the label that shows the answer.
+	 * @return java.awt.event.ActionListener
+	 */
+	ActionListener createTextFieldListener(String eventName, JComboBox<String> associatedComboBox, JComponent nextInFocus, JLabel answerBox);
+	
+	/**
+	 * Creates the matching combo box listener for the event. 
+	 * @param previousSelection the previous selected item of this combo box.
+	 * @param eventName the name of the corresponding event.
+	 * @param nextInFocus the component that gets the focus after the selection has been changed.
+	 * @param answerBox the label that shows the answer.
+	 * @return java.awt.event.ActionListener
+	 */
+	ActionListener createComboBoxListener(String previousSelection, String eventName, JComponent nextInFocus, JLabel... answerBox);
+	
+	/**
+	 * Creates the matching button listener for the event.
+	 * @param eventName the name of the corresponding event.
+	 * @param associatedComboBox the content of this combo box can be used.
+	 * @param associatedTextField the content of this text field can be used.
+	 * @param nextInFocus the component that gets the focus after the button has been triggered.
+	 * @param answerBox the label that shows the answer.
+	 * @return java.awt.event.ActionListener
+	 */
+	ActionListener createButtonListener(String eventName, JComboBox<String> associatedComboBox, JTextField associatedTextField, JComponent nextInFocus, JLabel... answerBox);
 }
