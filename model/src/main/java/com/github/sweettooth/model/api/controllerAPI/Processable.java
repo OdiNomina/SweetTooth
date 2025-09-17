452be7188d6412b96e59fd7117c2fec6061e2b10
Silke Dittmar <silkedittmar.ae@outlook.de>
2025-09-17 10:23:43 +0200

    Add java doc to model > controllerAPI > Processable.

--- model/src/main/java/com/github/sweettooth/model/api/controllerAPI/Processable.java
+++ model/src/main/java/com/github/sweettooth/model/api/controllerAPI/Processable.java
@@ -4,6 +4,21 @@ public interface Processable {
 	//This nested record is used as a response container (... no special reason).
 	public record Answer(String answer1, String answer2, String answer3) {}
 	
+	/**
+	 * 
+	 * @param stringInput
+	 * @param integerInput
+	 * @param doubleInput
+	 * @return Returns a string as the answer.
+	 */
 	String process(String stringInput, Integer integerInput, Double doubleInput);
+	
+	/**
+	 * 
+	 * @param stringInput
+	 * @param integerInput
+	 * @param doubleInput
+	 * @return Returns a record containing multiple strings as answers.
+	 */
 	Answer processMultipleAnswers(String stringInput, Integer integerInput, Double doubleInput);
 }
