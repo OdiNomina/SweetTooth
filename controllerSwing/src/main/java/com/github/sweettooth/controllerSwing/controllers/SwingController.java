530d21bb30c244bfcf76ce8358ef9a5f96a19710
Silke Dittmar <silkedittmar.ae@outlook.de>
2025-09-17 10:07:45 +0200

    Implement swing controller.

--- controllerSwing/src/main/java/com/github/sweettooth/controllerSwing/controllers/SwingController.java
+++ controllerSwing/src/main/java/com/github/sweettooth/controllerSwing/controllers/SwingController.java
@@ -1,20 +1,58 @@
 package com.github.sweettooth.controllerSwing.controllers;
 
+import java.awt.event.ActionListener;
 import java.util.Objects;
 
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 import com.github.sweettooth.controllerSwing.api.IController;
+import com.github.sweettooth.controllerSwing.buttonListener.ButtonListenerFactory;
+import com.github.sweettooth.controllerSwing.comboBoxListener.ComboBoxListenerFactory;
+import com.github.sweettooth.controllerSwing.textFieldListener.TextFieldListenerFactory;
 import com.github.sweettooth.model.api.IGameData;
 import com.github.sweettooth.model.api.controllerAPI.EventFactory;
 
 public class SwingController implements IController {
 	private IGameData gameData;
 	private EventFactory eventFactory;
+	private TextFieldListenerFactory textFieldListenerFactory;
+	private ButtonListenerFactory buttonListenerFactory;
+	private ComboBoxListenerFactory comboBoxListenerFactory;
 	
+	public EventFactory getEventFactory() {
+		return eventFactory;
+	}
+
+	public IGameData getGameModel() {
+		return gameData;
+	}
+
 	@Override
 	public IController initialize(IGameData gameData) throws NullPointerException {
 		this.gameData = Objects.requireNonNull(gameData);
 		eventFactory = EventFactory.getDefaultFactory();
+		textFieldListenerFactory = new TextFieldListenerFactory(this);
+		buttonListenerFactory = new ButtonListenerFactory(this);
+		comboBoxListenerFactory = new ComboBoxListenerFactory(this);
 		return this;
+	}
+	
+	@Override
+	public ActionListener createTextFieldListener(String eventName, JComboBox<String> associatedComboBox, JComponent nextInFocus, JLabel answerBox) {
+		return textFieldListenerFactory.create(eventName, associatedComboBox, nextInFocus, answerBox);
+	}
+
+	@Override
+	public ActionListener createComboBoxListener(String previousSelection, String eventName, JComponent nextInFocus, JLabel... answerBox) {
+		return comboBoxListenerFactory.create(previousSelection, eventName, nextInFocus, answerBox);
+	}
+
+	@Override
+	public ActionListener createButtonListener(String eventName, JComboBox<String> associatedComboBox, JTextField associatedTextBox, JComponent nextInFocus, JLabel... answerBox) {
+		return buttonListenerFactory.create(eventName, associatedComboBox, associatedTextBox, nextInFocus, answerBox);
 	}
 
 }
