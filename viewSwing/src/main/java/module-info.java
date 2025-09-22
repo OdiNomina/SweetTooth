84f3440556cb276683e896cf392edbb6680697ac
Silke Dittmar <silkedittmar.ae@outlook.de>
2025-09-22 16:46:04 +0200

    Add a new module to divide views into modules. Update example POMS_forMavenNature.

---
+++ viewSwing/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.github.sweettooth.viewSwing {
+	requires java.desktop;
+	requires com.github.sweettooth.shared;
+	requires com.github.sweettooth.model;
+	requires com.github.sweettooth.controllerSwing;
+}
