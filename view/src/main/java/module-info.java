23af1d08e88d133e9f3ede1233243e7e902f8887
Silke Dittmar <silkedittmar.ae@outlook.de>
2025-09-23 11:36:07 +0200

    Update POMs, example POMS_forMavenNature and module-info.java files.

--- view/src/main/java/module-info.java
+++ view/src/main/java/module-info.java
@@ -2,9 +2,8 @@ module com.github.sweettooth.view {
 	exports com.github.sweettooth.viewLanterna.api;
 	
 	requires java.desktop;
-	requires com.github.sweettooth.model;
 	requires com.github.sweettooth.controllerLanterna;
-	requires com.github.sweettooth.controllerSwing;
+	requires com.github.sweettooth.model;
 	requires com.github.sweettooth.shared;
 	requires com.googlecode.lanterna;
 }
--- viewSwing/src/main/java/module-info.java
+++ viewSwing/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.github.sweettooth.viewSwing {
 	exports com.github.sweettooth.viewSwing.api;
 	
 	requires java.desktop;
-	requires com.github.sweettooth.shared;
-	requires com.github.sweettooth.model;
 	requires com.github.sweettooth.controllerSwing;
+	requires com.github.sweettooth.model;
+	requires com.github.sweettooth.shared;
 }
