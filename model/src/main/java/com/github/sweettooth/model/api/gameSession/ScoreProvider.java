892ece2a76ea6280b10c14483b52789764636e2b
Silke Dittmar <silkedittmar.ae@outlook.de>
2025-11-04 10:57:33 +0100

    docs(model.api.gameSession): add java doc to ScoreProvider

--- model/src/main/java/com/github/sweettooth/model/api/gameSession/ScoreProvider.java
+++ model/src/main/java/com/github/sweettooth/model/api/gameSession/ScoreProvider.java
@@ -7,6 +7,9 @@ import com.github.sweettooth.model.gameSession.ScoreManager;
 
 public interface ScoreProvider {
 	
+	/**
+	 * This record is used as a DTO (data transfer object) because the data type used internally is not exported by the module.
+	 */
 	public record ScoreData(String name, String score) {}
 	
 	public static ScoreProvider createScoreProvider(IGlobalSettings globalSettings) {
@@ -14,7 +17,13 @@ public interface ScoreProvider {
 	}
 	
 	void addScore(String namePlayer, Double score);
+	
+	/**
+	 * Returns a list of ScoreData objects containing {@code name<String>} and {@code score<String>}.
+	 * @return a list of {@code ScoreData<String,String>}
+	 */
 	List<ScoreData> getScores();
+	
 	void readScoresFromFile();
 	void writeScoresToFile();
 }
